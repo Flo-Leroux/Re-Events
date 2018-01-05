8195be60efe89256b1b38f8bcc50494f9cb33854
Flo-Leroux <lerouxflorian1@gmail.com>
2018-01-05 23:33:06 +0100

    Update app.firebase.config.ts

--- src/app/app.firebase.config.ts
+++ src/app/app.firebase.config.ts
@@ -1,8 +1,8 @@
 export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyD0MmF-MK1JM6k3k2fcpHsbXPBtmz4Uc7U",
-    authDomain: "r8f6jdmzsvjy.firebaseapp.com",
-    databaseURL: "https://r8f6jdmzsvjy.firebaseio.com",
-    projectId: "r8f6jdmzsvjy",
-    storageBucket: "r8f6jdmzsvjy.appspot.com",
-    messagingSenderId: "494033147650"
+    apiKey: "YOUR_APP_INFO",
+    authDomain: "YOUR_APP_INFO",
+    databaseURL: "YOUR_APP_INFO",
+    projectId: "YOUR_APP_INFO",
+    storageBucket: "YOUR_APP_INFO",
+    messagingSenderId: "YOUR_APP_INFO"
 };
