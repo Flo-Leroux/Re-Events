842bbe99bb11c1b0c6b33b5a69e921e98618964f
Flo-Leroux <lerouxflorian1@gmail.com>
2018-01-05 23:45:02 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,12 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { LoginPage } from '../pages/login/login';
-import { EventsPage } from '../pages/events/events';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class ReEvents {
-  rootPage:any = EventsPage;
+  rootPage:any = LoginPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
